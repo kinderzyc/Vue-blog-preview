a7db2e35a7a1b8631de9174f07aecfc5470681e0
Your Namekinderzyc <75829215@qq.com>
2020-01-04 17:59:10 +0800

    first commit

---
+++ src/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+   <footer>
+       <p>© Vue个人博客 www.jirengu.com.cn All Rights Reserved</p>
+   </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #5e5e5e;
+  padding: 10px;
+  
+}
+p{
+font-size: 16px;
+text-align: center;
+}
+</style>
---
+++ src/pages/Create/template.vue
@@ -0,0 +1,25 @@
+<template>
+   <div id="create">
+    <h1>创建文章</h1>
+    <h3>文章标题</h3>
+    <el-input v-model="title" placeholder="请输入内容">></el-input>
+    <p class="msg">限30个字</p>
+    <h3>内容简介</h3>
+    <el-input v-model="description" type="textarea" :autosize="{ minRows: 2, maxRows: 6}" rows=3 placeholder="请输入内容"></el-input>
+    <p class="msg">限30个字</p>
+    <h3>文章内容</h3>
+    <el-input v-model="content" type="textarea" rows=20 :autosize="{ minRows: 4, maxRows: 30}" placeholder="请输入内容"></el-input>
+    <p class="msg">限60个字</p>
+    <p>
+        <label>是否展示到首页</label>
+        <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+    </p>
+    <el-button @click="onCreate" class="btt">立即创建</el-button>
+  </div>
+</template>
+
+<script src="./template.js"></script>
+
+<style lang="less" src="./template.less">
+ 
+</style>
