cd447192daddc28a937f19605c4d40449846c608
Your Namekinderzyc <75829215@qq.com>
2020-01-05 14:21:46 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: process.env.NODE_ENV === "production" && '/Vue-blog-preview/dist/' || '/' ,
 
     /**
      * Source Maps
